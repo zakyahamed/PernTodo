8f63448d6c7b855d537db1b99d256722bfbd35e1
Zaky Ahamed <128923249+zakyahamed@users.noreply.github.com>
2024-08-27 14:52:36 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const Pool = require('pg').Pool;
 
 const pool = new Pool({
     user: 'postgres',
-    password: 'zakynaleem123',
+    password: 'PASSWORD',
     host: 'localhost',
     port: 5432,
     database: 'perntodo'
